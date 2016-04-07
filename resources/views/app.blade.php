cac0ed54cbda735f37da337dc5126cc10b1c4985
ses10 <sesma@live.com>
2016-04-07 17:03:24 -0400

    add foundation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,10 @@
     <head>
         <title>@yield('title')</title>
 
+        <link href="{{ asset('css/foundation.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body>
+
 
             @yield('content')
 
